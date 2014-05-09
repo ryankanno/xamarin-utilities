95c0a4a0aab34cf2f2c7bcb8fc3c7408a1cda986
Ryan Kanno <ryankanno@localkinegrinds.com>
2014-05-09 08:02:56 -0400

    Initial commit - added Database helper

---
+++ src/Xamarin.Utilities.Core/Persistence/Database.cs
@@ -0,0 +1,24 @@
+﻿namespace Xamarin.Utilities.Core
+{
+	using System;
+	using SQLite.Net;
+
+	public class Database
+	{
+		private static readonly Lazy<Database> instance = new Lazy<Database>(() => new Database());
+
+		private Database ()
+		{
+		}
+
+		public static Database Instance 
+		{
+			get 
+			{
+				return instance.Value;
+			}	
+		}
+
+		public SQLiteConnection SqlConnection { get; private set; }
+	}
+}
